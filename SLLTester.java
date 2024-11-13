2736aa242cfe9be65897ed70b1ccd347101a70af
NadiaEs829 <nadia_amores@hotmail.com>
2024-11-13 19:27:36 +0000

    working

--- SLLTester.java
+++ SLLTester.java
@@ -1,5 +1,27 @@
+import java.util.LinkedList;
+
 class SLLTester {
   public static void main(String[] args) {
+    //create linkedlist 
+    LinkedList <String> list1 = new LinkedList <String>();
+    LinkedList <String> list2 = new LinkedList <String>();
+    LinkedList <String> list3 = new LinkedList <String>();
 
+    list1.add("H");
+    list1.add("a");
+    list1.add("p");
+    list1.add("p");
+    list1.add("y");
+
+    List2.add("A");
+    List2.add("A");
+    List2.add("A");
+    List2.add("A");
+    List2.add("A");
+
+    
+    list1.removeVowels();
+    System.out.println(list1);
+    
   } // end main
 } // end class
--- SuperLinkedList.java
+++ SuperLinkedList.java
@@ -2,15 +2,32 @@ import java.util.LinkedList;
 import java.util.Iterator;
 
 public class SuperLinkedList extends LinkedList<String> {
+    // this.
     public boolean removeVowels() {
-        return false;
+        Iterator <String> iter = this.listIterator();
+        boolean isVowel = false;
+        String vowels = "a,e,i,o,u,A,E,I,O,U";
+        while(iter.hasNext()){
+            if(iter.next().equals(vowels)){ 
+                iter.remove();
+                isVowel = true;
+            }//end if
+        }//end while
+        return isVowel;
+        //return false;
     }
 
     public boolean removeConsonants() {
+        boolean isVowel = false;
+        while(iter.hasNext()){
+            if(iter.next.equals())
+
+        }
         return false;
     }
 
     public LinkedList<String> removeDuplicates() {
+
         return null;
     }
 
@@ -20,7 +37,7 @@ public class SuperLinkedList extends LinkedList<String> {
     }
 
     public LinkedList<String> mix(LinkedList<String> list2) {
-
+        //use 2 iterators
         return null;
     }
 
